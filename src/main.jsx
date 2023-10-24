f0612c5546e6a02596f549d4b1381f738db3fb3b
Hasbi Rizqulloh <hasbirzq08@gmail.com>
2023-10-24 19:14:17 +0700

    adds

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './dist/css/main.css';
+import './dists/css/main.css';
 
 import { BrowserRouter } from 'react-router-dom';
 import ScrollTotop from './component/ScrollToTop.jsx';
